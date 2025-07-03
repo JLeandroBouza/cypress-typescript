6f8594b0a4e301edb17a9dc253777bbd4220f942
JLeandroBouza <jorge.leandrobouza@gmail.com>
2025-07-02 22:48:53 -0300

    Creacion del nuevo branch, agregado de las pruebas a loginTest

--- cypress/e2e/loginTest.cy.ts
+++ cypress/e2e/loginTest.cy.ts
@@ -1,19 +1,29 @@
 import { loginpage } from '../pages/login';
 import data  from '../fixtures/data.json';
 
+interface LoginData {
+  usuario_standard: string;
+  password_standard: string;
 
+}
+
+const loginData = data[0] as LoginData;
 
 
 describe('Pruebas Automatizadas de Login de Usuario: ', () => {
 
   beforeEach(() => {
     cy.visit('')
-    cy.fixture('data').as('data');
-  })
+  });
 
 
   it('Login Correcto de un usuario Standard', () => {
-    loginpage.login(data.usuario_standard, data.password_standard);
+    loginpage.login(loginData.usuario_standard, loginData.password_standard);
+    cy.url().should('include', '/inventory.html');
+  })
+
+  it('Login Correcto de un usuario bloqueado', () => {
+    loginpage.login(loginData.usuario_locked, loginData.password_locked);
     cy.url().should('include', '/inventory.html');
   })
 })
